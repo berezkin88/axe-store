e33def8f25da6f41017c56f6564c4116bd3b0251
berezkin88 <berezkin05@gmail.com>
2022-02-21 10:10:52 +0200

    added initiate payment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'axe-store';
+  url = '/v1/payments/instant-sepa-credit-transfers';
+  options = {
+    headers: {
+      Accept: '*/*',
+      'Content-Type': 'application/json',
+      'PSU-ID': 'user1',
+      'TPP-Redirect-URI': 'http://localhost:4200',
+      'TPP-Nok-Redirect-URI': 'http://localhost:8080',
+      'psu-ip-address': '1.1.1.1',
+      'x-request-id': uuidv4()
+    }
+  };
+
+  body = {
+    endToEndIdentification: 'WBG-123456789',
+    debtorAccount: {
+      currency: 'EUR',
+      iban: 'DE74908215670000000101'
+    },
+    instructedAmount: {
+      currency: 'EUR',
+      amount: '0.01'
+    },
+    creditorAccount: {
+      currency: 'EUR',
+      iban: 'DE04908215670000000100'
+    },
+    creditorAgent: 'AAAADEBBXXX',
+    creditorName: 'Frog',
+    creditorAddress: {
+      buildingNumber: '11',
+      townName: 'Berlin',
+      country: 'DE',
+      postCode: '10001',
+      streetName: 'Straße'
+    },
+    remittanceInformationUnstructured: 'Remittance String'
+  };
+
+  constructor(private http: HttpClient) { }
+
+  buyAxe(): void {
+    this.http.post(this.url, this.body, this.options)
+      .subscribe(response => console.log(response));
+
+    console.log('Axe bought');
+  }
 }
